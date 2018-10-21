fa6a6e40a9fcb50c800edacde1ad8d04da8fbf09
Lauren Perchitti <laurenperchitti@gmail.com>
2018-10-21 12:48:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ let store = {drivers: [], passengers: [], trips: []}
 let driverId = 0
 let passengerId = 0
 let tripId = 0
+
+class Driver {
+  constructor(name){
+  this.id = ++driverId
+  this.name = name
+  store.drivers.push(this)
+}
+
+trips(){
+  return store.trips.filter(i => i.driverId === this.id)
+}
+
+}
